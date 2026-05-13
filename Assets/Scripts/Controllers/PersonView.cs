d7982855376814f738ddf65a944e3fa4db7b55d5
Sandra Nilsen Brattli-Aspvik <sandrabrattliaspvik@gmail.com>
2026-05-13 20:48:47 +0200

    materials and prefabs

---
+++ Assets/Scripts/Controllers/PersonView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using CivilizationBuilder.Models;
+
+namespace CivilizationBuilder.Controllers
+{
+    public class PersonView : MonoBehaviour
+    {
+        private Person _person;
+
+        public void Initialize(Person person)
+        {
+            _person = person ?? throw new System.ArgumentNullException(nameof(person));
+            name = $"Person_{person.Name}";
+        }
+
+        public string PersonId => _person?.Id;
+    }
+}
